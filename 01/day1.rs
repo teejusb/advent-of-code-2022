b8b55cfecc853ef68c39676b551a8539fcd0829b
teejusb <5017202+teejusb@users.noreply.github.com>
2022-12-01 09:44:17 -0800

    Day 1

---
+++ 01/day1.rs
@@ -0,0 +1,29 @@
+use std::collections::BinaryHeap;
+use std::fs::File;
+use std::io::{BufRead, BufReader};
+
+fn main() {
+  let file = File::open("in.txt").expect("Cannot open file");
+  let reader = BufReader::new(file);
+  
+  let mut sum: i32 = 0;
+  let mut heap = BinaryHeap::new();
+  for line in reader.lines() {
+    let s = line.expect("Error reading the line");
+    if !s.is_empty() {
+      sum += s.trim().parse::<i32>().unwrap();
+    } else {
+      heap.push(sum);
+      sum = 0;
+    }
+  }
+  heap.push(sum);
+
+  sum = 0;
+  for _ in 1..4 {
+    if !heap.is_empty() {
+      sum += heap.pop().unwrap();
+    }
+  }
+  println!("{}", sum);
+}
